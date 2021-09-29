814d81e271e144e13928299e01a8e4fa8bf37a21
Constantinos Giannacoulis <c.giannacoulis@codehub.gr>
2021-09-29 21:17:28 +0300

    Added initial logging, main classes and Maven pom files.

---
+++ java-path-spring-app/src/main/java/gr/codelearn/spring/showcase/app/Application.java
@@ -0,0 +1,14 @@
+package gr.codelearn.spring.showcase.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot application initializer.
+ */
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
---
+++ java-path-spring-core/src/main/java/gr/codelearn/spring/showcase/core/CoreApplication.java
@@ -0,0 +1,14 @@
+package gr.codelearn.spring.showcase.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot application initializer.
+ */
+@SpringBootApplication
+public class CoreApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CoreApplication.class, args);
+	}
+}
