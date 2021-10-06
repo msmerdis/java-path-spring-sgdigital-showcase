4ca39df7459e67998c784f5ee0ab6bb2cc160fb7
Constantinos Giannacoulis <c.giannacoulis@codehub.gr>
2021-10-06 16:49:07 +0300

    Added annotation for identifying and parse custom property files.

--- java-path-spring-core/src/main/java/gr/codelearn/spring/showcase/core/CoreApplication.java
+++ java-path-spring-core/src/main/java/gr/codelearn/spring/showcase/core/CoreApplication.java
@@ -2,11 +2,13 @@ package gr.codelearn.spring.showcase.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 /**
  * Spring Boot application initializer.
  */
 @SpringBootApplication
+@PropertySource("classpath:custom.properties")
 public class CoreApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CoreApplication.class, args);
