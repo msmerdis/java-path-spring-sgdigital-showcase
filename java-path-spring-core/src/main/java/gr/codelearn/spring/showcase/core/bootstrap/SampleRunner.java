d609ba5e3c3b5efad4f441ce127590ac2cadebec
Constantinos Giannacoulis <c.giannacoulis@codehub.gr>
2021-10-06 17:05:20 +0300

    Sample Spring bean running only once during Spring content initialization.
    Signed-off-by: Constantinos Giannacoulis <c.giannacoulis@codehub.gr>

---
+++ java-path-spring-core/src/main/java/gr/codelearn/spring/showcase/core/bootstrap/SampleRunner.java
@@ -0,0 +1,25 @@
+package gr.codelearn.spring.showcase.core.bootstrap;
+
+import gr.codelearn.spring.showcase.core.base.AbstractLogComponent;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * Bootstrap class demonstrating the property retrieval from two distinctive sources. It runs only once during
+ * application initialization.
+ */
+@Component
+public class SampleRunner extends AbstractLogComponent implements CommandLineRunner {
+	@Value("${spring.application.name}")
+	private String applicationName;
+
+	@Value("${custom.property}")
+	private String customProperty;
+
+	@Override
+	public void run(final String... args) {
+		logger.info("Application named '{}' has a custom property having the value of '{}'.", applicationName,
+					customProperty);
+	}
+}
