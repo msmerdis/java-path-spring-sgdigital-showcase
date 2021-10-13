da033d92a8d4665c6a0c5f51d63ffe4cd5e2b016
Constantinos Giannacoulis <c.giannacoulis@codehub.gr>
2021-10-13 09:49:25 +0300

    Utilizing RequiredArgsConstructor annotation (creates a constructor having as parameters all attributes declared as private final) to replace the usage of Autowired annotation.
    Signed-off-by: Constantinos Giannacoulis <c.giannacoulis@codehub.gr>

--- java-path-spring-core/src/main/java/gr/codelearn/spring/showcase/core/controller/SampleController.java
+++ java-path-spring-core/src/main/java/gr/codelearn/spring/showcase/core/controller/SampleController.java
@@ -1,13 +1,18 @@
 package gr.codelearn.spring.showcase.core.controller;
 
 import gr.codelearn.spring.showcase.core.base.AbstractLogComponent;
+import gr.codelearn.spring.showcase.core.component.DependentBean;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class SampleController extends AbstractLogComponent {
+	private final DependentBean dependentBean;
+
 	@GetMapping("/")
 	public String sayHello() {
-		return "Hello from our sample Spring Boot";
+		return "Hello from our sample Spring Boot. " + dependentBean.getSampleText();
 	}
 }
