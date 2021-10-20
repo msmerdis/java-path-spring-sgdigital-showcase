e385006271ac8ebaa252f8b5fcfcf4242d8b10e2
Constantinos Giannacoulis <c.giannacoulis@codehub.gr>
2021-10-20 13:55:15 +0300

    Created an abstracted controller class to hold the implementation of all core business actions and its exposure as RESTful services.
    Signed-off-by: Constantinos Giannacoulis <c.giannacoulis@codehub.gr>

--- java-path-spring-app/src/main/java/gr/codelearn/spring/showcase/app/controller/ProductController.java
+++ java-path-spring-app/src/main/java/gr/codelearn/spring/showcase/app/controller/ProductController.java
@@ -1,24 +1,20 @@
 package gr.codelearn.spring.showcase.app.controller;
 
 import gr.codelearn.spring.showcase.app.domain.Product;
+import gr.codelearn.spring.showcase.app.service.BaseService;
 import gr.codelearn.spring.showcase.app.service.ProductService;
-import gr.codelearn.spring.showcase.app.transfer.ApiResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/products")
 @RequiredArgsConstructor
-public class ProductController {
+public class ProductController extends AbstractController<Product> {
 	private final ProductService productService;
 
-	@GetMapping()
-	public ResponseEntity<ApiResponse<List<Product>>> findAll() {
-		return ResponseEntity.ok(ApiResponse.<List<Product>>builder().data(productService.findAll()).build());
+	@Override
+	public BaseService<Product, Long> getBaseService() {
+		return productService;
 	}
 }
