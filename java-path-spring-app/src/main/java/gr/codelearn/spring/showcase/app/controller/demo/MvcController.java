9a77a3fe790d167df37163d844cfc4b1001d9179
Ioannis Daniil <jondan97@gmail.com>
2021-10-22 09:59:29 +0300

    Added cookie functionality

--- java-path-spring-app/src/main/java/gr/codelearn/spring/showcase/app/controller/demo/MvcController.java
+++ java-path-spring-app/src/main/java/gr/codelearn/spring/showcase/app/controller/demo/MvcController.java
@@ -15,8 +15,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.math.BigDecimal;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
@@ -26,39 +31,34 @@ public class MvcController {
 	final ProductService productService;
 
 	@GetMapping
-	public String index(Model model){
+	public String index(Model model) {
 		Category category = Category.builder().description("Food").build();
-		Product product = Product.builder().name("Tomatoes").category(category).price(BigDecimal.ONE).serial("AA110").build();
+		Product product = Product.builder().name("Tomatoes").category(category).price(BigDecimal.ONE).serial("AA110")
+								 .build();
 		model.addAttribute("name", "Ioannis");
 		model.addAttribute("product", product);
 		return "index";
 	}
 
 	@GetMapping("/redirect")
-	public String redirectionExample(){
+	public String redirectionExample() {
 		return "redirect:/mvc";
 	}
 
 	@GetMapping("/ourProducts")
-	public String findAllProduct(Model model){
+	public String findAllProduct(Model model) {
 		List<Product> allProducts = productService.findAll();
 		model.addAttribute("products", allProducts);
 		return "ourProducts";
 	}
 
-	@GetMapping("/about")
-	public String about(){
-		return "about";
-	}
-
-
 	@GetMapping("/registerProduct")
-	public String registerProduct(){
+	public String registerProduct() {
 		return "registerProduct";
 	}
 
 	@GetMapping("/condition")
-	public String condition(Model model){
+	public String condition(Model model) {
 		boolean result = true;
 		model.addAttribute("result", result);
 		Product product = null;
@@ -66,12 +66,23 @@ public class MvcController {
 		return "demo/condition";
 	}
 
+	@GetMapping("/about")
+	public String about(HttpServletRequest request, HttpServletResponse response) {
+		createCookie(request, response);
+		return "about";
+	}
 
-
+	private void createCookie(HttpServletRequest request, HttpServletResponse response) {
+		Cookie cookie = Arrays.stream(request.getCookies()).filter(c -> c.getName().equals("counter")).findAny()
+									  .orElse(new Cookie("counter", "0"));
+		int counterValue = Integer.parseInt(cookie.getValue());
+		cookie.setValue(String.valueOf(++counterValue));
+		response.addCookie(cookie);
+	}
 
 	@GetMapping("/findYourProduct")
-	public String findYourProduct(Model model, @RequestParam(required = false) Integer productIndex){
-		if (productIndex != null){
+	public String findYourProduct(Model model, @RequestParam(required = false) Integer productIndex) {
+		if (productIndex != null) {
 			List<Product> allProducts = productService.findAll();
 			Product product = allProducts.get(productIndex);
 			model.addAttribute("product", product);
@@ -80,7 +91,7 @@ public class MvcController {
 	}
 
 	@PostMapping("/findYourProduct")
-	public String executeFindYourProduct1(Model model, @RequestParam Integer productIndex){
+	public String executeFindYourProduct1(Model model, @RequestParam Integer productIndex) {
 		List<Product> allProducts = productService.findAll();
 		Product product = allProducts.get(productIndex);
 		model.addAttribute("product", product);
@@ -88,7 +99,7 @@ public class MvcController {
 	}
 
 	@GetMapping("/findYourProduct/{index}")
-	public String executeFindYourProduct2(Model model, @PathVariable Integer index){
+	public String executeFindYourProduct2(Model model, @PathVariable Integer index) {
 		List<Product> allProducts = productService.findAll();
 		Product product = allProducts.get(index);
 		model.addAttribute("product", product);
--- java-path-spring-app/src/main/resources/freemarker/about.ftl
+++ java-path-spring-app/src/main/resources/freemarker/about.ftl
@@ -1,3 +1,4 @@
+<html>
 <#import "/spring.ftl" as spring />
 <#import  "partial/siteTemplate.ftl" as siteTemplate>
 <head>
@@ -8,5 +9,26 @@
 <p><@spring.message "owner.name"/></p>
 <p><@spring.message "owner.food"/></p>
 <p><@spring.message "owner.decision"/></p>
+
+<p id="timesVisitedParagraph"></p>
+
 <@siteTemplate.footer/>
 </body>
+<#-- Simple script which reads a "counter" cookie and replaces a paragraph with a text notifying the user about
+the cookie (more specifically: the times he has visited the page) -->
+<script>
+	const cookieValue = getCookie("counter");
+	document.getElementById("timesVisitedParagraph").textContent = "You have visited this page " + cookieValue + " " +
+			"times!";
+
+	// Function copied from the internet, basically access the wanted cookie
+	function getCookie(name) {
+		let cookie = {};
+		document.cookie.split(';').forEach(function (el) {
+			let [k, v] = el.split('=');
+			cookie[k.trim()] = v;
+		})
+		return cookie[name];
+	}
+</script>
+</html>
